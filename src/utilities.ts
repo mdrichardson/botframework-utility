eb38f42cd16c3d997fea9a584bb8a807b8bdcb00
mdrichardson <v-micric@microsoft.com>
2019-04-16 12:11:11 -0700

    - right-click emulator launch works - botSettings keys are fuzzy matched => normalized

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 
 const dotenv = require('dotenv');
+const FuzzyMatching = require('fuzzy-matching');
 
 export function createEmulatorUri(url: string, domain: string = 'livechat', action: string = 'open'): vscode.Uri {
     return vscode.Uri.parse(`bfemulator://${domain}.${action}?botUrl=${url}`);
@@ -12,19 +13,27 @@ export async function getBotSettings(): Promise<object> {
     const appsettingsJsonFile = await vscode.workspace.findFiles(`**/appsettings.json`, null, 1);
     let botSettings: any = {};
     if (dotenvFile[0]) {
-        dotenv.config({ path: dotenvFile[0].fsPath });
         const json = dotenv.parse(fs.readFileSync(dotenvFile[0].fsPath));
         for (const key in json) {
-            botSettings[key] = json[key];
+            const normalizedKey = normalizeEnvKeys(key);
+            botSettings[normalizedKey] = json[key];
         }
     } 
     else if (appsettingsJsonFile[0]) {
         const raw = String(fs.readFileSync(appsettingsJsonFile[0].fsPath));
         const json = JSON.parse(raw);
         for (const key in json) {
-            process.env[key] = json[key];
-            botSettings[key] = json[key];
+            const normalizedKey = normalizeEnvKeys(key);
+            botSettings[normalizedKey] = json[key];
         }
     }
     return botSettings;
+}
+
+function normalizeEnvKeys(key: string): string {
+    const fm = new FuzzyMatching([
+        'MicrosoftAppId',
+        'MicrosoftAppPassword'
+    ]);
+    return fm.get(key).value;
 }
